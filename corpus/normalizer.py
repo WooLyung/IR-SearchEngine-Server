bccecc621deeeadcd9b279b9bdda055b552b4895
WooLyung <vlfkdydtk@naver.com>
2022-05-27 10:04:35 +0900

    add query normalizing

--- corpus/normalizer.py
+++ corpus/normalizer.py
@@ -1,23 +1,30 @@
 from encodings import utf_8
 from konlpy.tag import Okt
 import re
+import sys
 
 okt = Okt()
+sys.stdout.reconfigure(encoding='utf-8')
 
-with open('corpus.txt', 'r', encoding='utf8') as file:
-    with open('nor_corpus.txt', 'w', encoding='utf8') as output:
-        lines = file.readlines()
-        first = True
+if len(sys.argv) == 1:
+    with open('corpus.txt', 'r', encoding='utf8') as file:
+        with open('nor_corpus.txt', 'w', encoding='utf8') as output:
+            lines = file.readlines()
+            first = True
 
-        for line in lines:
-            if re.search('<title>.*</title>', line):
-                segment = line.replace('<title>', '').replace('</title>', '').strip().split('. ')
-                docid = segment[0]
-                title = segment[1]
+            for line in lines:
+                if re.search('<title>.*</title>', line):
+                    segment = line.replace('<title>', '').replace('</title>', '').strip().split('. ')
+                    docid = segment[0]
+                    title = segment[1]
 
-                output.write(('' if first else '\n') + docid + ' ' + title + ' ')
-                first = False
+                    output.write(('' if first else '\n') + docid + ' ' + title + ' ')
+                    first = False
 
-            elif line.strip() != '':
-                result = list(map(lambda x: x[0].strip(), okt.pos(line, norm=True, stem=True)))
-                output.write(' '.join(result))
+                elif line.strip() != '':
+                    result = list(map(lambda x: x[0].strip(), okt.pos(line, norm=True, stem=True)))
+                    output.write(' '.join(result))
+else:
+    argv = ' '.join(sys.argv[1:])
+    result = list(map(lambda x: x[0].strip(), okt.pos(argv.strip(), norm=True, stem=True)))
+    print(' '.join(result))
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@ mod retrieve;
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::process::Command;
+use std::str::from_utf8;
+use subprocess::Exec;
 use index::Indexer;
 
 fn handle_client(mut stream: TcpStream) {
     let mut data = [0 as u8; 50];
     while match stream.read(&mut data) {
         Ok(size) => {
-            println!("{}", std::str::from_utf8(&data[0..size]).unwrap());
+            println!("{}", from_utf8(&data[0..size]).unwrap());
             stream.write(&data[0..size]).unwrap();
             size != 0
         },
@@ -23,14 +26,21 @@ fn handle_client(mut stream: TcpStream) {
 }
 
 fn main() -> std::io::Result<()> {
-    let mut indexer = Indexer::new();
-    indexer.read("corpus/nor_corpus.txt")
-        .index()
-        .sort()
-        .tfidf()
-        .normalize()
-        .print()
-        .save();
+    // let mut indexer = Indexer::new();
+    // indexer.read("corpus/nor_corpus.txt")
+    //     .index()
+    //     .sort()
+    //     .tfidf()
+    //     .normalize()
+    //     .print()
+    //     .save();
+
+    let output = Command::new("python")
+        .args(["./corpus/normalizer.py", "나는 자랑스러운 태극기 앞에 자유롭고 정의로운 대한민국의 무궁한 영광을 위하다."])
+        .output()
+        .expect("error!!");
+    let query = String::from_utf8(output.stdout).unwrap();
+    println!("{}", &query);
 
     let listener = TcpListener::bind("127.0.0.1:3997")?;
     for stream in listener.incoming() {
